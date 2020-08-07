5cf23a58084cfae5f660ac341b33c4dd969276f6
lis154 <lis154@mail.ru>
2020-08-07 15:27:49 +0300

    2

---
+++ src/main/java/test/service/FilmService.java
@@ -0,0 +1,13 @@
+package test.service;
+
+import test.model.Film;
+
+import java.util.List;
+
+public interface FilmService {
+    List<Film> allFilms();
+    void add(Film film);
+    void delete(Film film);
+    void edit(Film film);
+    Film getById(int id);
+}
